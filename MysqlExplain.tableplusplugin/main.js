6a41dc957af3f2e7ced31bcb7980d764bf2d2e8d
tpetry <github@tpetry.me>
2023-07-06 13:40:16 +0200

    wip

---
+++ MysqlExplain.tableplusplugin/main.js
@@ -0,0 +1,60 @@
+'use strict';
+
+import { executeQuery } from './library/helper';
+
+function handleResults(context, sql, resExplainJson, resExplainTraditional, resExplainTree, resVersion, resWarnings) {
+    for (const res of [resExplainJson, resExplainTraditional, resVersion, resWarnings]) {
+        if (res instanceof Error) {
+            context.alert('Error', res.message);
+
+            return;
+        }
+    }
+
+    const api = {
+        query: sql,
+        version: resVersion[0].version,
+        explain_traditional: resExplainTraditional,
+        explain_json: resExplainJson[0].EXPLAIN,
+        explain_tree: (resExplainTree instanceof Error) ? null : resExplainTree[0].EXPLAIN,
+        warnings: resWarnings,
+    };
+    SystemService.insertToClipboard(JSON.stringify(api, null, 4));
+    SystemService.notify('Query executed', 'EXPLAIN information collected');
+}
+
+global.onRun = function(context) {
+    if (!['MariaDB', 'MySQL'].includes(context.driver())) {
+        context.alert('Error', 'Only MySQL and MariaDB databases are supported.');
+
+        return;
+    }
+
+    const queryEditor = context.currentQueryEditor();
+    if (queryEditor === null) {
+        context.alert('Error', 'You have to select a query in the SQL Query editor.');
+
+        return;
+    }
+
+    const sql = queryEditor.currentSelectedString();
+
+    // The behaviour of the TablePlus query execution API is weird...
+    // The results are sent to the closure (indicating async logic) but the next query will only be executed after the
+    // current one is finished (indicating sync logic). Therefore, the code needs to be developed in async way but not
+    // be complicated by pipelining the SQL queries.
+    let resExplainJson = null;
+    let resExplainTraditional = null;
+    let resExplainTree = null;
+    let resVersion = null;
+    let resWarnings = null;
+    executeQuery(context, 'SELECT VERSION() as version ', (res) => resVersion = res);
+    executeQuery(context, context.driver() === 'MariaDB' ? `EXPLAIN EXTENDED ${sql}` : `EXPLAIN FORMAT=TRADITIONAL ${sql}`, (res) => resExplainTraditional = res);
+    executeQuery(context, 'SHOW WARNINGS', (res) => resWarnings = res);
+    executeQuery(context, `EXPLAIN FORMAT=JSON ${sql}`, (res) => resExplainJson = res);
+    executeQuery(context, `EXPLAIN FORMAT=TREE ${sql}`, (res) => {
+        resExplainTree = res;
+
+        handleResults(context, sql, resExplainJson, resExplainTraditional, resExplainTree, resVersion, resWarnings);
+    });
+};
--- MysqlExplain.tableplusplugin/webpack.config.js
+++ MysqlExplain.tableplusplugin/webpack.config.js
@@ -4,10 +4,10 @@ const path = require('path');
 const config = {
     entry: './main.js',
     output: {
-    	path: path.resolve(__dirname, './'),
-    	filename: './format.js'
+    	path: path.resolve(__dirname, './build'),
+    	filename: './plugin.js',
     },
-    mode: 'production'
+    mode: 'production',
 };
 
 module.exports = config;
